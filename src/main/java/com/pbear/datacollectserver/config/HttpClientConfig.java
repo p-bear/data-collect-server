feffeeaa10f73cf77e0d166f3e6f59396da03c2f
p-bear <friendship41@cuk.edu>
2023-04-02 21:36:16 +0900

    Initial commit

---
+++ src/main/java/com/pbear/datacollectserver/config/HttpClientConfig.java
@@ -0,0 +1,7 @@
+package com.pbear.datacollectserver.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpClientConfig {
+}
---
+++ src/test/java/com/pbear/datacollectserver/DataCollectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbear.datacollectserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCollectServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
